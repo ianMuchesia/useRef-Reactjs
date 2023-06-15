35355e9ccdb6317602968ef32f994c061e2644b1
ianMuchesia <ianmuchesia@yahoo.com>
2023-06-15 10:49:06 +0300

    images

--- src/pages/Images.jsx
+++ src/pages/Images.jsx
@@ -1,23 +1,50 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { image_1, image_2, image_3 } from '../assets'
 
 const ImageJSON = [
     {
       name: 'image_1',
-      image: 'image_1.jpeg'
+      image: image_1,
     },
     {
       name: 'image_2',
-      image: 'image_2.jpeg'
+      image: image_2
     },
     {
       name: 'image_3',
-      image: 'image_3.jpeg'
+      image: image_3,
     },
   ]
+
+
+
+
   
 const Images = () => {
+
+
+   const [ index , setIndex] = useState(0)
+
+   useEffect(() => {
+    const intervalID = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex === 2 ? 0 : prevIndex + 1));
+    }, 100000);
+
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, []);
+
+  console.log(index);
   return (
-    <div>Images</div>
+    <div>
+        
+       
+
+      <div className="">
+        <img src={ImageJSON[index].image} alt="" />
+      </div>
+    </div>
   )
 }
 
